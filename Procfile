103b49e5095b2a80a7c802dabc89acdd97596bb9
Aasf-Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2022-03-24 13:05:11 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,3 @@
+worker: python3 -m Vjii_Robot
+web: python3 -m Vjii_Robot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE Vjii_Robot
+:: Enables virtual env mode and then starts gorgeous
+env\scripts\activate.bat && py -m Vjii_Robot
